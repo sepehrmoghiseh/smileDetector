497b65ddaf2222108fc053dd035754c0fa9e2c82
sepehr <sepehrmogiseh@gmail.com>
2021-03-11 21:01:37 +0330

    very simple smile-Detector

---
+++ main.py
@@ -0,0 +1,24 @@
+'''
+a very simple smile-detector for images with use of cv2 and haarcascade
+@author: Sepehr-Moghiseh
+@date:03/11/2021
+@version:01
+'''
+import cv2
+
+dir = input("enter the path of the Image! : \n  ")
+
+image = cv2.imread(dir)
+
+smile_cascade = cv2.CascadeClassifier(r'haarcascade\haarcascade_smile.xml')
+
+smiles = smile_cascade.detectMultiScale(image, scaleFactor=2, minNeighbors=10)
+
+for (sx, sy, sw, sh) in smiles:
+    cv2.rectangle(image, (sx, sy), ((sx + sw), (sy + sh)), (4, 120, 112), 4)
+
+cv2.imshow("Smile Detected", image)
+
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
